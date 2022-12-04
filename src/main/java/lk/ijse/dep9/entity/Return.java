4bb960814fcff5114133bd978e533fcd66c9d628
chalana <amithchalana19@gmail.com>
2022-12-04 13:48:55 +0530

    Return entity completion

--- src/main/java/lk/ijse/dep9/entity/Return.java
+++ src/main/java/lk/ijse/dep9/entity/Return.java
@@ -1,4 +1,20 @@
 package lk.ijse.dep9.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Return implements SuperEntity {
+    private Date date;
+    private ReturnPK returnPK;
+
+    public Return(Date date, int issueId, String isbn) {
+        this.date = date;
+        this.returnPK = new ReturnPK(issueId, isbn);
+    }
 }
--- src/main/java/lk/ijse/dep9/entity/ReturnPK.java
+++ src/main/java/lk/ijse/dep9/entity/ReturnPK.java
@@ -1,6 +1,15 @@
 package lk.ijse.dep9.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReturnPK implements Serializable {
+    private int issueId;
+    private String isbn;
 }
