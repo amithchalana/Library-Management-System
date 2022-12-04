543f55183f95baf2fa57bde8bf2455bd7470affe
chalana <amithchalana19@gmail.com>
2022-12-04 12:31:45 +0530

    Create the DAO layer interfaces hierarchy

---
+++ src/main/java/lk/ijse/dep9/dao/custom/BookDAO.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep9.dao.custom;
+
+import lk.ijse.dep9.dao.CrudDAO;
+
+public interface BookDAO extends CrudDAO {
+}
---
+++ src/main/java/lk/ijse/dep9/dao/custom/ReturnDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep9.dao.custom;
+
+import lk.ijse.dep9.dao.CrudDAO;
+import lk.ijse.dep9.dao.SuperDAO;
+
+public interface ReturnDAO extends CrudDAO {
+}
